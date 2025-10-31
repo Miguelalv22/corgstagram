b62d9acff5f635b8672c431daaef95f3c9fca4df
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-10-31 12:18:20 -0600

    Styles

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,3 +3,12 @@
         <div class="Footer-copy">© 2025 CORGSTAGRAM FROM MILEZ</div>
     </div>
 </div>
+
+<style>
+    .Footer {
+        font-size: 11px;
+        letter-spacing: 1px;
+        font-weight: normal;
+        color: #c7c7c7;
+    }
+</style>
